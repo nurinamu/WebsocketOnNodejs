c45c2f7820f5f6320bf86e05206c1368bccbeaf2
leewonjae <nurinamu@gmail.com>
2014-05-08 18:25:41 +0900

    create the simple video chat demo.

--- websocket/webserver.js
+++ websocket/webserver.js
@@ -1,11 +1,48 @@
 var fs = require("fs");
 var http = require("http");
+var url = require("url");
 
-http.createServer(function(req, res) {
-	fs.readFile('websocket_client.html', function(err, data) {
-		res.writeHead(200, {'Content-Type' : 'text/html'});
-		res.end(data);
-	});
-}).listen(8080, function() {
+http.createServer(htmlRoute).listen(8080, function() {
 	console.log("Server running");
 });
+
+
+function htmlRoute(req, res){
+	var src = getPath(req.url);
+	fs.readFile(src, function(err, data) {
+		if(err){
+			res.writeHead(404, {});
+			res.end();
+		}else{
+			var headers = {};
+				headers['Content-Type']=filterContentType(src);
+			res.writeHead(200, headers);
+			res.end(data);
+		}
+	});
+}
+
+function filterContentType(fileName_){
+	var idx = fileName_.lastIndexOf('.');
+	if(idx > 0){
+		switch(fileName_.substr(idx+1).toLowerCase()){
+			case 'js':
+				return "text/javascript";
+		}
+	}
+}
+
+function getPath(reqUrl_){
+	switch(url.parse(reqUrl_).pathname){
+		case "/":
+			return 'websocket_client.html';
+		case "/webrtc":
+			return 'webrtc_demo.html';
+		default:
+			if(reqUrl_.charAt(0) == '/'){
+				return reqUrl_.substring(1);
+			}else{
+				return reqUrl_;
+			}
+	}
+}
